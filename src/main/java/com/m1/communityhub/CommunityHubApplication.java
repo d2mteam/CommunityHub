ff54dfdca8a0623212167d946080e1e405c96776
2302 <23020110@vnu.edu.com>
2026-01-24 21:50:46 +0700

    First commit. Generate project

---
+++ src/main/java/com/m1/communityhub/CommunityHubApplication.java
@@ -0,0 +1,13 @@
+package com.m1.communityhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/m1/communityhub/CommunityHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.m1.communityhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
